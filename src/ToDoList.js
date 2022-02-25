448e69a3f5cd48fe780ad5bff754f2ddc7c8760f
RedMarshmellow <ahmedhaniaminmohamed@gmail.com>
2022-02-25 03:48:56 +0300

    Integrated ToDo App Widget

---
+++ src/ToDoList.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Logo from "./logo.svg";
+import ToDoItem from "./ToDoItem";
+import "./ToDoList.css";
+
+const ToDoList = () => {
+    const [list, setList] = useState([
+        { id: 1, text: "clean the house" },
+        { id: 2, text: "buy milk" },
+    ]);
+    const [toDo, setToDo] = useState("");
+    const [showError, setShowError] = useState(false);
+
+    const generateId = () => {
+        if (list && list.length) {
+            return Math.max(...list.map((t) => t.id)) + 1;
+        } else {
+            return 1;
+        }
+    };
+
+    const displayError = () => {
+        setShowError(true);
+        const clearTimer = setTimeout(() => setShowError(false), 3000);
+        return () => clearTimeout(clearTimer);
+    };
+    const createNewToDoItem = () => {
+        if (!toDo) {
+            displayError();
+            return;
+        }
+        const newId = generateId();
+        const newToDo = { id: newId, text: toDo };
+        setList([...list, newToDo]);
+        setToDo("");
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            createNewToDoItem();
+        }
+    };
+
+    const handleInput = (e) => {
+        setToDo(e.target.value);
+    };
+
+    const deleteItem = (id) => {
+        setList(list.filter((item) => item.id !== id));
+    };
+
+    return (
+        <div className="ToDo">
+            <img className="Logo" src={Logo} alt="React logo" />
+            <h1 className="ToDo-Header">React To Do</h1>
+            <div className="ToDo-Container">
+                <div className="ToDo-Content">
+                    {list.map((item) => {
+                        return <ToDoItem key={item.id} item={item} deleteItem={deleteItem} />;
+                    })}
+                </div>
+
+                <div className="ToDoInput">
+                    <input
+                        type="text"
+                        placeholder="I need to..."
+                        value={toDo}
+                        onChange={handleInput}
+                        onKeyPress={handleKeyPress}
+                    />
+                    <button className="ToDo-Add" onClick={createNewToDoItem}>
+                        +
+                    </button>
+                </div>
+                <div className="ToDo-ErrorContainer">{showError && <p>Please enter a todo!</p>}</div>
+            </div>
+        </div>
+    );
+};
+
+export default ToDoList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import WeatherApp from './WeatherApp';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+      <WeatherApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
